04ca0b641b06a176cc32e5cbd4fd8a7e2310d70e
EderPG <l20120144@morelia.outlook.com>
2025-02-06 13:42:59 -0600

    add adapter

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AxiosAdapter } from './adapters/axios.adapter';
 
-@Module({})
+@Module({
+    providers: [AxiosAdapter], //se importa el adaptador como un provider
+    exports: [AxiosAdapter] //se exporta para que pueda usarlo otro modulo que lo necesite
+})
 export class CommonModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import axios, { AxiosInstance } from 'axios';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); // delete * from pokemon
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650'); //el numero de solicitudes que hara, en este caso 650 pokemon
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650', //el numero de solicitudes que hara, en este caso 650 pokemon
+    );
     const pokemonToInsert: { name: string; no: number }[] = [];
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/'); // divide la url que nos da en partes para saber que posicion esta el dato necesario
